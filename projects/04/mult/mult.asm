3bc489a1bcdb46f690d372ca4d5c5a6f6d82be01
Nathan Perkins <nperkins487@gmail.com>
2020-06-07 15:58:09 -0700

    Project 04/mult done

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,45 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// R2 = 0
+// for (i = 0; i < R1; i++) {
+//   R2 = R2 + R0
+// }
+// while(1) {}
+
+// R2 = 0
+	@R2
+	M=0
+
+// i = 0
+	@i
+	M=0
+
+(LOOP)
+// if (i < R1)
+	@i
+	D=M
+	@R1
+	D=D-M
+	@END
+	D;JGE
+
+// R2 = R2 + R0
+	@R0
+    D=M
+	@R2
+	M=D+M
+
+// i++
+	@i
+	M=M+1
+
+// goto LOOP
+	@LOOP
+	0;JMP
+
+(END)
+// Program End
+	@END
+	0;JMP
