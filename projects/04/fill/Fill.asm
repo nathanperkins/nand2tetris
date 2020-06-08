4d5fd99061590ca0ff50ff257d6ba6823a413390
Nathan Perkins <nperkins487@gmail.com>
2020-06-07 17:05:49 -0700

    Project 04/fill done

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,67 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// Start by making the screen white
+	@WHITE
+	0; JMP
+
+(TOP)
+// Write value to all positions
+
+// for (i = 0; i < 32; i++)
+
+// i = 0
+	@i
+	M=0
+
+// addr = &SCREEN
+	@SCREEN
+	D=A
+	@addr
+	M=D 
+
+(FILL)
+// i < 32
+	@i
+	D=M
+	@8192 // number of words in the screen
+	D=D-A
+	@CHECK
+	D; JGE
+
+// Mem[addr] = value
+	@value
+	D=M
+	@addr
+	A=M
+	M=D
+
+// i++
+	@i
+	M=M+1
+// addr++
+	@addr
+	M=M+1
+	@FILL
+	0; JMP
+
+// check keyboard register
+(CHECK)
+	@KBD
+	D=M
+	@BLACK
+	D; JNE
+
+// white, if button is pressed
+(WHITE)
+	@value
+	M=0
+	@TOP
+	0; JMP
+
+// black, otherwise
+(BLACK)
+	@value
+	M=-1
+	@TOP
+	0; JMP
